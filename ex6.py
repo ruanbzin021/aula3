1a6b9668fdfe7eac2e6c88ed888c49d778e929b2
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-18 18:38:21 -0300

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -4,11 +4,7 @@ int2 = int(input("Informe outro numero inteiro: "))
 
 int3 = int(input("Informe um numero inteiro: "))
 
-if int1 > int2 and int1 > int3:
-    print(f"{int1} é maior")
-
-elif int2 > int1 and int2 > int3:
-    print(f"{int2} é maior")    
-
-elif int3 > int1 and int3 > int2:
-    print(f"{int3} é maior")
+numero = [int1, int2, int3]
+valor_max = max(numero)
+valor_min = min(numero)
+print(f"O Maior é: {valor_max}")
