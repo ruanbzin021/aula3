95afcd275fa452ed3e7309a43a7d5f0abb8edceb
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-18 17:59:32 -0300

    Atualizar o exe5.py

--- exe5.py
+++ exe5.py
@@ -1,1 +1,13 @@
-oi
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"\nMédia alcançada: {media:.2f}")
+
+if media == 10.0:
+    print("Aluno aprovado com distinção!")
+elif media >= 7.0:
+    print("Aluno aprovado.")
+else:
+    print("Aluno reprovado.")
