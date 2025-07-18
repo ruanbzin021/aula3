ccd2bae42e74752d0294a17e03c9b62aae672e59
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-18 18:59:30 -0300

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,9 @@
+int1 = int(input("Informe um numero inteiro: "))
+
+int2 = int(input("Informe outro numero inteiro: "))
+
+int3 = int(input("Informe um numero inteiro: "))
+
+numero = [int1, int2, int3]
+numero.sort(reverse=True)
+print(numero)
