d95147fd6654177a99c476b9137f3deb2febd5de
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-18 16:30:23 -0300

    Atualizar o exe4.py

--- exe4.py
+++ exe4.py
@@ -1,1 +1,11 @@
-oi
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+  
+letra = input("Digite uma letra: ").lower()
+
+if letra.isalpha():  # .isalpha() Verifica se é uma letra
+    if letra in 'aeiou':
+        print("Vogal")
+    else:
+        print("Consoante")
+else:
+    print("Entrada inválida! Por favor, digite apenas uma letra.")
