a949d4430ed0fd7a262bed5421ac23e13c60c2c3
Ruan <ruan.ruanzinho777@gmail.com>
2025-07-19 13:48:25 -0300

    Criar ex13.py

---
+++ ex13.py
@@ -0,0 +1,24 @@
+dia = int(input("Digite um número relacionado aos dias da semana (1 a 7): "))
+if dia == 1:
+  print("Domingo")
+    
+elif dia == 2:
+  print("Segunda-Feira")
+  
+elif dia == 3:
+  print("Terça-Feira")
+  
+elif dia == 4:
+   print("Quarta-Feira")
+  
+elif dia == 5:
+  print("Quinta-Feira")
+  
+elif dia == 6:
+  print("Sexta-Feira")
+  
+elif dia == 7:
+  print("Sábado")
+  
+else:
+    print("Digite um número de 1 a 7")
